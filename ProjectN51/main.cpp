87dc702783e28ef272902d6e8cbe053ac40a2941
pc-laptop <pc-laptop@pc-laptops-macbook-pro.local>
2018-10-24 09:12:16 -0500

    Initial Commit

---
+++ ProjectN51/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectN51
+//
+//  Created by pc-laptop on 10/24/18.
+//  Copyright © 2018 Ingrid Guerrero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
